11b8823768f97ae5b980e27c9ad6110146c1fdec
dimmy2000 <dimsamoylenko@gmail.com>
2021-06-02 21:25:26 +0300

    added templates, static

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        "author": "dimmy2000",
+        "title": "Post #1",
+        "content": "Hello World!",
+        "date_posted": "June 1, 2021",
+    },
+    {
+        "author": "John Doe",
+        "title": "Post #2",
+        "content": "This is the second post content",
+        "date_posted": "June 2, 2021",
+    },
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    """Home page view."""
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    """About page view."""
+    return render(request, "blog/about.html")
