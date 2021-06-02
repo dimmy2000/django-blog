126b1e9d703af587cbb310c48b6db4a1f18b7ce5
dimmy2000 <dimsamoylenko@gmail.com>
2021-06-03 00:01:23 +0300

    db and migrations

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    """Example of Post in blog."""
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        """Inner class for adding metadata to model."""
+
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    def __str__(self):
+        """Class instance representation."""
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        "author": "dimmy2000",
-        "title": "Post #1",
-        "content": "Hello World!",
-        "date_posted": "June 1, 2021",
-    },
-    {
-        "author": "John Doe",
-        "title": "Post #2",
-        "content": "This is the second post content",
-        "date_posted": "June 2, 2021",
-    },
-]
+from .models import Post
 
 
 def home(request):
     """Home page view."""
     context = {
-        "posts": posts,
+        "posts": Post.objects.all(),
     }
     return render(request, "blog/home.html", context)
 
